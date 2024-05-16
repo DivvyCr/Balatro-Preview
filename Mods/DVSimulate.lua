332ab59b4630487d853f01587317513c0e390e3b
CUexter <1155143289@link.cuhk.edu.hk>
2024-05-16 22:58:04 +0800

    fix: midas mask and vampire not calculating properly with bonus card

--- Mods/DVSimulate.lua
+++ Mods/DVSimulate.lua
@@ -1567,7 +1567,7 @@ function DV.SIM.set_ability(card_data, center)
       order = center.order or nil,
       forced_selection = card_data.ability and card_data.ability.forced_selection or nil,
       perma_bonus = card_data.ability and card_data.ability.perma_bonus or 0,
-      bonus = (card_data.ability.bonus or 0) + (center.config.bonus or 0)
+		bonus = (center.config.bonus or 0),
    }
 end
 
