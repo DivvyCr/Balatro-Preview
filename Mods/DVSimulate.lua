DivvyCr/Balatro-Preview

commit 6b9297214bde71ad844e742f024c3080b82a1e2e

Remove redundant updates to hands_played and hands_left

Mods/DVSimulate.lua
@@ -29,8 +29,6 @@ DV.SIM = {
    orig = {
       --- Table to store game data that gets modified during simulation:
       random_data = {}, -- G.GAME.pseudorandom
-      hands_played = 0, -- G.GAME.current_round.hands_played
-      hands_left = 0,   -- G.GAME.current_round.hands_left
       hand_data = {}    -- G.GAME.hands
    },
 
@@ -192,26 +190,18 @@ function DV.SIM.manage_state(save_or_restore)
 
    if save_or_restore == "SAVE" then
       DVSO.random_data = copy_table(G.GAME.pseudorandom)
-      DVSO.hands_played = G.GAME.current_round.hands_played
-      DVSO.hands_left = G.GAME.current_round.hands_left
       DVSO.hand_data = copy_table(G.GAME.hands)
       return
    end
 
    if save_or_restore == "RESTORE" then
       G.GAME.pseudorandom = DVSO.random_data
-      G.GAME.current_round.hands_played = DVSO.hands_played
-      G.GAME.current_round.hands_left = DVSO.hands_left
       G.GAME.hands = DVSO.hand_data
       return
    end
 end
 
 function DV.SIM.update_state_variables()
-   -- Increment hands played this round:
-   G.GAME.current_round.hands_played = G.GAME.current_round.hands_played + 1
-   G.GAME.current_round.hands_left = G.GAME.current_round.hands_left - 1
-
    -- Increment poker hand played this run/round:
    local hand_info = G.GAME.hands[DV.SIM.env.scoring_name]
    hand_info.played = hand_info.played + 1

commit 6ebd194a30054d94a40a8214116c702aaffb0b14

Fix simulation of Dusk and Acrobat
Addresses regressions introduced by 6b92972

Mods/DVSimulate.lua
@@ -575,7 +575,8 @@ DV.SIM.JOKERS = {
    end,
    simulate_dusk = function(joker_obj, context)
       if context.cardarea == G.play and context.repetition then
-         if G.GAME.current_round.hands_left == 0 then
+         -- Note: Checking against 1 is needed as hands_left is not decremented as part of simulation
+         if G.GAME.current_round.hands_left == 1 then
             DV.SIM.add_reps(joker_obj.ability.extra)
          end
       end
@@ -1089,7 +1090,8 @@ DV.SIM.JOKERS = {
    end,
    simulate_acrobat = function(joker_obj, context)
       if context.cardarea == G.jokers and context.global then
-         if G.GAME.current_round.hands_left == 0 then
+         -- Note: Checking against 1 is needed as hands_left is not decremented as part of simulation
+         if G.GAME.current_round.hands_left == 1 then
             DV.SIM.x_mult(joker_obj.ability.extra)
          end
       end