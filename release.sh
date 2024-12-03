44e3231630f2f7358d025206713b3e71b7d2744e
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2024-12-03 18:44:51 +0000

    Fix injection targets; fix release script

--- release.sh
+++ release.sh
@@ -3,7 +3,10 @@ mkdir release/DVPreview/
 mkdir release/DVSimulate/
 
 cp -r src/* release/DVPreview
+cp lovely.toml release/DVPreview
+
 cp -r $DVSIM_PATH/src/* release/DVSimulate
+cp $DVSIM_PATH/lovely.toml release/DVSimulate
 
 cd release/
 zip -r DOWNLOAD.zip *
