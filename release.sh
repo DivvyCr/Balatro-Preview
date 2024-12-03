0700e94d763d5e3397a97d75f2063e87c3362ed7
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2024-12-03 18:26:04 +0000

    Rename release zip file

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ cp -r src/* release/DVPreview
 cp -r $DVSIM_PATH/src/* release/DVSimulate
 
 cd release/
-zip -r DVPreview.zip *
-mv DVPreview.zip ..
+zip -r DOWNLOAD.zip *
+mv DOWNLOAD.zip ..
 cd ..
 
 rm -rf release/
